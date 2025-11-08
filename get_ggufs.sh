a9495c62f20a48ebc48ff2916f51495ab29d0a47
Daniil Iskhakov <shkiper325@gmail.com>
2025-11-08 19:11:54 +0300

    Added links to yandexgpt and tlite ggufs

---
+++ get_ggufs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd "$HOME"
+wget https://huggingface.co/yandex/YandexGPT-5-Lite-8B-instruct-GGUF/resolve/main/YandexGPT-5-Lite-8B-instruct-Q4_K_M.gguf
+wget https://huggingface.co/t-tech/T-pro-it-2.0-GGUF/resolve/main/T-pro-it-2.0-Q4_K_M.gguf
